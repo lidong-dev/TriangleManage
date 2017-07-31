a1987553d458aa7d9fe877f8603179a8b2933918
triangleUser <jijiyy11@126.com>
2017-07-31 21:49:49 +0800

    ss

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -12,7 +12,7 @@ namespace TestProject
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        { 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
